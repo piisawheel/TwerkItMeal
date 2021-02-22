9c96b8fec8156f10e5814bd1aa691088bb2afc74
jjuleff <jjuleff@levementum.com>
2021-02-21 23:09:34 -0700

    Modified twerk code. Moved 50% chance to per growable, instead of a 50% chance of growing everything. Added dirt block to growable code and 10% chance to turn it into grass. Added grass to growables and added 5 percent chance per grass block of spawning bamboo. Adjusted range to 2 x 1 (5x3x5 square around player)

--- src/main/java/com/ticticboooom/twerkitmeal/TwerkItMeal.java
+++ src/main/java/com/ticticboooom/twerkitmeal/TwerkItMeal.java
@@ -76,19 +76,39 @@ public class TwerkItMeal {
 
             ServerWorld world = (ServerWorld) event.player.world;
             ServerPlayerEntity player = (ServerPlayerEntity) event.player;
-            if (playerCrouchCount >= TwerkConfig.minCrouchesToApplyBonemeal && world.getRandom().nextDouble() <= 0.5) {
+            if (playerCrouchCount >= TwerkConfig.minCrouchesToApplyBonemeal) {
                 crouchCount.put(uuid, 0);
+                double nextRandom = 0;
                 List<BlockPos> growables = getNearestBlocks(world, player.getPosition());
                 for (BlockPos growablePos : growables) {
                     BlockState blockState = world.getBlockState(growablePos);
                     if (!FilterListHelper.shouldAllow(blockState.getBlock().getRegistryName().toString())) {
                         continue;
                     }
+                    nextRandom = world.getRandom().nextDouble();
                     if (blockState.hasProperty(CropsBlock.AGE)) {
+                    	if (nextRandom > .5) { 
+                    		continue;
+                    	}
                         int growth = blockState.get(CropsBlock.AGE);
                         world.setBlockState(growablePos, blockState.with(CropsBlock.AGE, growth < 7 ? growth + 1 : 7));
                     } else if (blockState.getBlock() instanceof IGrowable) {
-                        BoneMealItem.applyBonemeal(new ItemStack(Items.BONE_MEAL), world, growablePos, player);
+                    	if (blockState.getBlock() == Blocks.GRASS_BLOCK) {
+                    		BlockPos above = growablePos.up();
+                    		BlockState aboveState = world.getBlockState(above);
+                    		//5 percent chance to add bamboo to empty grass block
+                    		//5 percent (other side of random tick) to apply bonemeal to grass.
+                    		if (aboveState.getBlock() == Blocks.AIR && nextRandom <= 0.05) {
+                    			world.setBlockState(above, Blocks.BAMBOO_SAPLING.getDefaultState());
+                    		} else if (nextRandom >= 0.95) {
+                        		BoneMealItem.applyBonemeal(new ItemStack(Items.BONE_MEAL), world, growablePos, player);
+                    		}
+                    	} else if (nextRandom <= .5) {
+                    		BoneMealItem.applyBonemeal(new ItemStack(Items.BONE_MEAL), world, growablePos, player);
+                    	}
+                    	//10 percent chance to turn dirt into grass.
+                    } else if (blockState.getBlock() == Blocks.DIRT && nextRandom <= 0.1) {
+                    	world.setBlockState(growablePos, Blocks.GRASS_BLOCK.getDefaultState());
                     }
                     world.spawnParticle(player, ParticleTypes.HAPPY_VILLAGER, false, growablePos.getX() + world.rand.nextDouble(), growablePos.getY() + world.rand.nextDouble(), growablePos.getZ() + world.rand.nextDouble(), 10, 0, 0, 0, 3);
                 }
@@ -97,11 +117,11 @@ public class TwerkItMeal {
 
         private List<BlockPos> getNearestBlocks(World world, BlockPos pos) {
             List<BlockPos> list = new ArrayList<>();
-            for (int x = -5; x <= 5; x++)
+            for (int x = -2; x <= 2; x++)
                 for (int y = -2; y <= 2; y++)
-                    for (int z = -5; z <= 5; z++) {
+                    for (int z = -2; z <= 2; z++) {
                         Block block = world.getBlockState(new BlockPos(x + pos.getX(), y + pos.getY(), z + pos.getZ())).getBlock();
-                        if (block instanceof IGrowable) {
+                        if (block instanceof IGrowable || block == Blocks.DIRT) {
                             if (FilterListHelper.shouldAllow(block.getRegistryName().toString())) {
                                 list.add(new BlockPos(x + pos.getX(), y + pos.getY(), z + pos.getZ()));
                             }
--- src/main/java/com/ticticboooom/twerkitmeal/config/CommonConfig.java
+++ src/main/java/com/ticticboooom/twerkitmeal/config/CommonConfig.java
@@ -14,21 +14,26 @@ public class CommonConfig {
     public CommonConfig(ForgeConfigSpec.Builder builder) {
         List<String> defaultBlackList = new ArrayList<>();
         defaultBlackList.add("minecraft:netherrack");
-        defaultBlackList.add("minecraft:grass_block");
         defaultBlackList.add("minecraft:warped_nylium");
         defaultBlackList.add("minecraft:crimson_nylium");
         defaultBlackList.add("minecraft:tall_grass");
         defaultBlackList.add("botanypots");
         defaultBlackList.add("gaiadimension");
+        List<String> defaultWhiteList = new ArrayList<>();
+        defaultWhiteList.add("minecraft:dirt");
+        defaultWhiteList.add("minecraft:grass_block");
+        defaultWhiteList.add("minecraft:bamboo_sapling");
+        defaultWhiteList.add("minecraft:bamboo");
+        defaultWhiteList.add("minecraft:grass_block");
         showParticles = builder.comment("Whether to show particles or not when crouching to grow things")
                 .define("showParticles", true);
-        useWhitelist = builder.comment("whether to enable the whitelist this does not disable the blacklist")
-                .define("useWhiteList", false);
-        blackList = builder.comment("growables to disable crouching on")
+        useWhitelist = builder.comment("Whitelist Or Blacklist (exclusively 1 or the other).")
+                .define("useWhiteList", true);
+        blackList = builder.comment("Growables to disable crouching on")
                 .define("blacklist", defaultBlackList);
-        whitelist = builder.comment("growables to enable crouching on (only works if 'useWhiteList' is true and will give exclisivity to those growables) ")
-                .define("whitelist", new ArrayList<>());
-        minCrouchesToApplyBonemeal = builder.comment("the minimum number of crouches before the bonemeal is applied (bonemeal is applied randomly so this will not be exact)")
-                .defineInRange("minCrouchesToApplyBonemeal", 5, 0, Integer.MAX_VALUE);
+        whitelist = builder.comment("Growables to enable crouching on")
+                .define("whitelist", defaultWhiteList);
+        minCrouchesToApplyBonemeal = builder.comment("The minimum number of crouches before the bonemeal is applied.\nThere is a 50% chance any allowed item will be bonemealed, within a 5x5 square around the player.\nThere is a 10% chance dirt will spawn grass\nA 5% chance grass will spawn bamboo\nA 5% chance to bonemeal any particular grass block(tall grass and flowers).")
+                .defineInRange("minCrouchesToApplyBonemeal", 1, 0, Integer.MAX_VALUE);
     }
 }
